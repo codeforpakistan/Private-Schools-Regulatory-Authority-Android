5c87716ed1038515c84874c6daaf519d7341a508
mshahzaib374 <35091886+mshahzaib374@users.noreply.github.com>
2019-01-03 14:54:06 +0500

    Webview SSL Error removed

--- app/src/main/java/com/psra/complaintsystem/activities/WebviewFragment.java
+++ app/src/main/java/com/psra/complaintsystem/activities/WebviewFragment.java
@@ -2,10 +2,12 @@ package com.psra.complaintsystem.activities;
 
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,8 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+
+import com.psra.complaintsystem.R;
 
 
 /**
@@ -35,6 +39,7 @@ public class WebviewFragment extends Fragment {
 // specify the url of the web page in loadUrl function
         progressBar = new ProgressDialog(getActivity());
         progressBar.setMessage("Please wait...");
+        progressBar.setCanceledOnTouchOutside(false);
         if (savedInstanceState == null) {
             webView.loadUrl("https://psra.gkp.pk/");
             webView.getSettings().setJavaScriptEnabled(true);
@@ -59,8 +64,68 @@ public class WebviewFragment extends Fragment {
             return true;
         }
 
-        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-            handler.proceed();
+        /*public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("You are proceeding to an unsafe website");
+            builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.proceed();
+                }
+            });
+            builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.cancel();
+                }
+            });
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+        }*/
+
+        public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            String mensaje;
+            switch (error.getPrimaryError()) {
+                case SslError.SSL_DATE_INVALID:
+                    mensaje = "SSL date invalid";
+                    break;
+                case SslError.SSL_EXPIRED:
+                    mensaje = "SSL expired";
+                    break;
+                case SslError.SSL_IDMISMATCH:
+                    mensaje = "SSL Id mismatch";
+                    break;
+                case SslError.SSL_INVALID:
+                    mensaje = "SSL invalid";
+                    break;
+                case SslError.SSL_NOTYETVALID:
+                    mensaje = "SSL not yet valid";
+                    break;
+                case SslError.SSL_UNTRUSTED:
+                    mensaje = "SSL untrusted";
+                    break;
+                default:
+                    mensaje = String.valueOf(R.string.notification_error_ssl_cert_invalid);
+            }
+
+
+            builder.setTitle("SSL Certificate Error");
+            builder.setMessage(mensaje);
+            builder.setPositiveButton("Continue anyway", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.proceed();
+                }
+            });
+            builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.cancel();
+                }
+            });
+            final AlertDialog dialog = builder.create();
+            dialog.show();
         }
 
 
